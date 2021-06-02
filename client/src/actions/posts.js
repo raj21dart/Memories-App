7f1069d25a11911f4e986d6bdc8b7c632d9d89f1
rj21dart <rajsen89611@gmail.com>
2021-06-02 19:35:23 +0530

    'version-1'

---
+++ client/src/actions/posts.js
@@ -0,0 +1,58 @@
+import { CREATE, FETCH_ALL, UPDATE, DELETE } from '../constants/actionTypes'
+import * as api from '../api'
+
+// Action Creators
+export const getPosts = () => async (dispatch) => {
+    try {
+        const { data } = await api.fetchPosts()
+        console.log('Action : Data', data )
+        dispatch({ type: FETCH_ALL, payload: data })
+
+    } catch (error) {
+        console.log(error.message);
+    }
+} 
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const { data } = await api.createPost(post)
+        console.log('Action/posts: ', data);
+        dispatch({type: CREATE, payload: data})
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const updatePost = (id, post) => async(dispatch) => {
+
+    try {
+        const { data } = await api.updatePost(id, post)
+        console.log('action - updatePost');
+
+        dispatch({ type: UPDATE, payload: data})
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id)
+
+        dispatch({ type: DELETE, payload: id})
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const likePost = (id) => async (dispatch) => {
+    try {
+        const { data } = await api.likePost(id)
+
+        dispatch({ type: UPDATE, payload: data})
+    } catch (error) {
+        console.log(error);
+    }
+}
